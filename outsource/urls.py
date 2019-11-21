dee66d487b7c16d10f98ddfb1af8b11598d16975
Kiral <2952951121599@qq.com>
2019-11-21 19:52:58 +0800

    模板修改

--- outsource/urls.py
+++ outsource/urls.py
@@ -13,6 +13,18 @@ urlpatterns = [
     url(r'^index$', views.index),
     # http://127.0.0.1:8000/projects
     url(r'^projects$', views.projects),
+    # http://127.0.0.1:8000/projects/detail/projects_id  项目详情页
+    url(r'^projects/detail/(\d+)$', views.detail_projects),
+    # http://127.0.0.1:8000/cases
+    url(r'^cases$', views.cases),
+    # http://127.0.0.1:8000/cases/detail/projects_id  案例详情页
+    url(r'^cases/detail/(\d+)$', views.cases_projects),
+    # http://127.0.0.1:8000/help
+    url(r'^help$', views.help_menu),
+    # http://127.0.0.1:8000/publish
+    url(r'^publish$', views.publish),
     # http://127.0.0.1:8000/reg
     url(r'^reg$', views.reg),
+    # http://127.0.0.1:8000/login
+    url(r'^login$', views.login),
 ]
--- outsource/views.py
+++ outsource/views.py
@@ -12,5 +12,29 @@ def projects(request):
     return render(request, 'outsource/projects.html')
 
 
+def detail_projects(request):
+    return render(request, 'outsource/detail_projects.html')
+
+
+def cases(request):
+    return render(request, 'outsource/cases.html')
+
+
+def cases_projects(request):
+    return render(request, 'outsource/detail_cases.html')
+
+
+def help_menu(request):
+    return render(request, 'outsource/help.html')
+
+
+def publish(request):
+    return render(request, 'outsource/publish.html')
+
+
 def reg(request):
     return render(request, 'outsource/reg.html')
+
+
+def login(request):
+    return render(request, 'outsource/login.html')
